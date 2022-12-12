03bc861a26dbab9cc7d1d311f6987b5ba7a36146
ttjh1234 <ttjh1234@naver.com>
2022-12-12 15:45:18 +0900

    Prompt Tuning Experiment

---
+++ p-tuning-v2/README.md
@@ -0,0 +1,27 @@
+# P-tuning v2
+
+Natural Language Processing Prompt Tuning Experiment
+
+# Citation
+
+'''console
+@article{DBLP:journals/corr/abs-2110-07602,
+  author    = {Xiao Liu and
+               Kaixuan Ji and
+               Yicheng Fu and
+               Zhengxiao Du and
+               Zhilin Yang and
+               Jie Tang},
+  title     = {P-Tuning v2: Prompt Tuning Can Be Comparable to Fine-tuning Universally
+               Across Scales and Tasks},
+  journal   = {CoRR},
+  volume    = {abs/2110.07602},
+  year      = {2021},
+  url       = {https://arxiv.org/abs/2110.07602},
+  eprinttype = {arXiv},
+  eprint    = {2110.07602},
+  timestamp = {Fri, 22 Oct 2021 13:33:09 +0200},
+  biburl    = {https://dblp.org/rec/journals/corr/abs-2110-07602.bib},
+  bibsource = {dblp computer science bibliography, https://dblp.org}
+}
+'''
---
+++ p-tuning-v2/model/sequence_classification.py
@@ -0,0 +1,364 @@
+import torch
+from torch._C import NoopLogger
+import torch.nn
+import torch.nn.functional as F
+from torch import Tensor
+from torch.nn import CrossEntropyLoss, MSELoss, BCEWithLogitsLoss
+
+from transformers import BertModel, BertPreTrainedModel
+from transformers import RobertaModel, RobertaPreTrainedModel
+from transformers.modeling_outputs import SequenceClassifierOutput, BaseModelOutput, Seq2SeqLMOutput
+
+from model.prefix_encoder import PrefixEncoder
+#from model.deberta import DebertaModel, DebertaPreTrainedModel, ContextPooler, StableDropout
+
+import copy
+
+class BertForSequenceClassification(BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.config = config
+
+        self.bert = BertModel(config)
+        self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = torch.nn.Linear(config.hidden_size, config.num_labels)
+
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
+            Labels for computing the sequence classification/regression loss. Indices should be in :obj:`[0, ...,
+            config.num_labels - 1]`. If :obj:`config.num_labels == 1` a regression loss is computed (Mean-Square loss),
+            If :obj:`config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        loss = None
+        if labels is not None:
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+class BertPrefixForSequenceClassification(BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.config = config
+        self.bert = BertModel(config)
+        self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = torch.nn.Linear(config.hidden_size, config.num_labels)
+
+        for param in self.bert.parameters():
+            param.requires_grad = False
+        
+        self.pre_seq_len = config.pre_seq_len
+        self.n_layer = config.num_hidden_layers
+        self.n_head = config.num_attention_heads
+        self.n_embd = config.hidden_size // config.num_attention_heads
+
+        self.prefix_tokens = torch.arange(self.pre_seq_len).long()
+        self.prefix_encoder = PrefixEncoder(config)
+
+        bert_param = 0
+        for name, param in self.bert.named_parameters():
+            bert_param += param.numel()
+        all_param = 0
+        for name, param in self.named_parameters():
+            all_param += param.numel()
+        total_param = all_param - bert_param
+        print('total param is {}'.format(total_param)) # 9860105
+    
+    def get_prompt(self, batch_size):
+        prefix_tokens = self.prefix_tokens.unsqueeze(0).expand(batch_size, -1).to(self.bert.device)
+        past_key_values = self.prefix_encoder(prefix_tokens)
+        # bsz, seqlen, _ = past_key_values.shape
+        past_key_values = past_key_values.view(
+            batch_size,
+            self.pre_seq_len,
+            self.n_layer * 2, 
+            self.n_head,
+            self.n_embd
+        )
+        past_key_values = self.dropout(past_key_values)
+        past_key_values = past_key_values.permute([2, 0, 3, 1, 4])
+        # past_key_values : n_layer*2, batch_size, n_head, pre_seq_len, n_embd
+        
+        #discard_num=[17,11,10,15,16,18,19,20,20,21,20,22,25,25,26,26,26,23,22,24,19,16,20,19]
+        #for i,j in zip(range(24),discard_num):
+        #    past_key_values[(i*2):((i+1)*2),:,:,:j,:]=0
+                
+        past_key_values = past_key_values.split(2)
+        return past_key_values
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        batch_size = input_ids.shape[0]
+        past_key_values = self.get_prompt(batch_size=batch_size)
+    
+        discard_num=[34,22,19,30,33,36,38,39,40,43,41,44,49,50,52,51,53,46,45,49,38,31,39,38]
+        
+        out=self.bert.embeddings(input_ids,token_type_ids)
+        
+        if output_attentions:
+            total_attentions=torch.zeros(batch_size,0,self.n_head,128,128+self.pre_seq_len).to(self.bert.device)
+        
+            for i,j in zip(range(self.n_layer),discard_num):
+                
+                prefix_attention_mask=torch.cat([torch.zeros(batch_size,j).to(self.bert.device),torch.ones(batch_size,self.pre_seq_len-j).to(self.bert.device)],dim=1)
+                attention_mask2 = torch.cat((prefix_attention_mask, attention_mask), dim=1)
+                dtype=torch.float16
+                attention_mask2=attention_mask2[:,None,None,:]
+                extended_attention_mask = attention_mask2.to(dtype=dtype)  
+                extended_attention_mask = (1.0 - extended_attention_mask) * -1e10
+                
+                out,attention_prob=self.bert.encoder.layer[i](out,attention_mask=extended_attention_mask,past_key_value=past_key_values[i],output_attentions=output_attentions)
+                
+                if output_attentions:
+                    attention_prob=attention_prob.unsqueeze(1)
+                    total_attentions=torch.cat([total_attentions,attention_prob],dim=1)
+        
+        else:
+            total_attentions=None
+            for i,j in zip(range(self.n_layer),discard_num):
+                
+                prefix_attention_mask=torch.cat([torch.zeros(batch_size,j).to(self.bert.device),torch.ones(batch_size,self.pre_seq_len-j).to(self.bert.device)],dim=1)
+                attention_mask2 = torch.cat((prefix_attention_mask, attention_mask), dim=1)
+                dtype=torch.float16
+                attention_mask2=attention_mask2[:,None,None,:]
+                extended_attention_mask = attention_mask2.to(dtype=dtype)  
+                extended_attention_mask = (1.0 - extended_attention_mask) * -1e10
+                
+                out=self.bert.encoder.layer[i](out,attention_mask=extended_attention_mask,past_key_value=past_key_values[i],output_attentions=output_attentions)
+                out=out[0]
+        
+        
+        pooled_output=self.bert.pooler(out)
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        loss = None
+        if labels is not None:
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+        
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            attentions=total_attentions,
+        )
+
+
+
+'''
+class BertPrefixForSequenceClassification(BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.config = config
+        self.bert = BertModel(config)
+        self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = torch.nn.Linear(config.hidden_size, config.num_labels)
+
+        for param in self.bert.parameters():
+            param.requires_grad = False
+        
+        self.pre_seq_len = config.pre_seq_len
+        self.n_layer = config.num_hidden_layers
+        self.n_head = config.num_attention_heads
+        self.n_embd = config.hidden_size // config.num_attention_heads
+
+        self.prefix_tokens = torch.arange(self.pre_seq_len).long()
+        self.prefix_encoder = PrefixEncoder(config)
+
+        bert_param = 0
+        for name, param in self.bert.named_parameters():
+            bert_param += param.numel()
+        all_param = 0
+        for name, param in self.named_parameters():
+            all_param += param.numel()
+        total_param = all_param - bert_param
+        print('total param is {}'.format(total_param)) # 9860105
+    
+    def get_prompt(self, batch_size):
+        prefix_tokens = self.prefix_tokens.unsqueeze(0).expand(batch_size, -1).to(self.bert.device)
+        past_key_values = self.prefix_encoder(prefix_tokens)
+        # bsz, seqlen, _ = past_key_values.shape
+        past_key_values = past_key_values.view(
+            batch_size,
+            self.pre_seq_len,
+            self.n_layer * 2, 
+            self.n_head,
+            self.n_embd
+        )
+        past_key_values = self.dropout(past_key_values)
+        past_key_values = past_key_values.permute([2, 0, 3, 1, 4])
+        # past_key_values : n_layer*2, batch_size, n_head, pre_seq_len, n_embd
+        
+        #discard_num=[17,11,10,15,16,18,19,20,20,21,20,22,25,25,26,26,26,23,22,24,19,16,20,19]
+        #for i,j in zip(range(24),discard_num):
+        #    past_key_values[(i*2):((i+1)*2),:,:,:j,:]=0
+                
+        past_key_values = past_key_values.split(2)
+        return past_key_values
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        batch_size = input_ids.shape[0]
+        past_key_values = self.get_prompt(batch_size=batch_size)
+        prefix_attention_mask = torch.ones(batch_size, self.pre_seq_len).to(self.bert.device)
+        attention_mask = torch.cat((prefix_attention_mask, attention_mask), dim=1)
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            past_key_values=past_key_values,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        loss = None
+        if labels is not None:
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+'''
